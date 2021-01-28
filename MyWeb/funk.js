c6ef2c85f4c55935a4abbeb99da60cd8205987ac
RafiOnTheSea <77859124+RafiOnTheSea@users.noreply.github.com>
2021-01-28 10:28:07 +0100

    Add files via upload

---
+++ MyWeb/funk.js
@@ -0,0 +1,10 @@
+function enterlink () {
+
+    var xyz = document.getElementsByClassName("MusicLinks");
+    var linkstring = "";  
+
+    console.log("give me a link");
+
+    linkstring = prompt("Input:");
+} 
+return enterlink;
